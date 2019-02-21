c03d5af4a70915c9f5d09ddf3cda8981d96aab20
Alina <alex.alina.rusu@gmail.com>
2019-02-21 16:29:03 +0100

    added routes in App to render page sections/components

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import React, { Component } from 'react';
 import './App.css';
 import { Route } from 'react-router-dom';
 import Navbar from './components/navbar/Navbar';
+import IntroSection from './components/intro/IntroSection';
+import ValuePropSection from './components/valueProposition/ValuePropSection';
+import HowItWorks from './components/howItWorks/HowItWorks';
+// import Footer from './components/footer/Footer';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navbar />
+        <Route path="/" exact component={IntroSection} />
+        <Route path="/value-proposition" exact component={ValuePropSection} />
+        <Route path="/how-it-works" exact component={HowItWorks}/>
+        {/* <Footer /> */}
       </div>
     );
   }
