39980128e21f9e129076ad37f6fc30bc54cdbb26
Alina <alex.alina.rusu@gmail.com>
2019-02-26 14:06:45 +0100

    added the rest of the styles to IntroSections and ValuePropSection

--- src/components/intro/IntroSection.js
+++ src/components/intro/IntroSection.js
@@ -6,9 +6,11 @@ class IntroSection extends PureComponent {
   render() {
     return (
       <div className="intro-container">
-        <h1 className="main-title">multiply</h1>
-        <h2 className="tag-line">Make a pattern from one drawing in two minutes.</h2>
-        <SignupFormContainer />
+        <div className="intro-text">
+          <h1 className="main-title">multiply</h1>
+          <h2 className="tag-line">Make a pattern from one drawing in two minutes.</h2>
+          <SignupFormContainer />
+        </div>
       </div>
     );
   }
--- src/components/valueProposition/ValuePropSection.js
+++ src/components/valueProposition/ValuePropSection.js
@@ -1,32 +1,33 @@
 import React, { PureComponent } from 'react';
 import './ValuePropSection.css';
-import { FaCheckCircle } from "react-icons/fa";
+import { FaHatWizard, FaRegCopy } from "react-icons/fa";
+import { MdAccessTime } from "react-icons/md";
+import { IoIosFlash } from "react-icons/io";
+import { GiFairyWand } from "react-icons/gi";
 
 class ValuePropSection extends PureComponent {
   render() {
     return (
-      <div className="values-container">
-        <h2>Value Proposition</h2>
-        <div className="center-values">
-          <ul className="values-list">
-            <li className="li-value-item">
-              <FaCheckCircle />
-              <p>Save time on scenes with repetitive elements</p>
-            </li>
-            <li className="li-value-item">
-              <FaCheckCircle />
-              <p>Turn your designs into reusable templates</p>
-            </li>
-            <li className="li-value-item">
-              <FaCheckCircle />
-              <p>Quickly make variations of your work</p>
-            </li>
-            <li className="li-value-item">
-              <FaCheckCircle />
-              <p>Gain productivity superpowers by adding a  <br></br>powerful tool to your workflow</p>
-            </li>
-          </ul>
-        </div>
+      <div className="value-container">
+        <h2 className="value-title">Value Proposition</h2>
+        <ul className="value-list">
+          <li className="li-value-item">
+            <MdAccessTime className="li-icon" />
+            <p>Save time on scenes with repetitive elements</p>
+          </li>
+          <li className="li-value-item">
+            <FaRegCopy className="li-icon" />
+            <p>Turn your designs into reusable templates</p>
+          </li>
+          <li className="li-value-item">
+            <IoIosFlash className="li-icon" />
+            <p>Quickly make variations of your work</p>
+          </li>
+          <li className="li-value-item">
+            <GiFairyWand className="li-icon" />
+            <p>Gain productivity superpowers by adding <br></br>a powerful tool to your workflow</p>
+          </li>
+        </ul>
       </div>
     );
   }
