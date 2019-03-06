3dad9510579bc2abd981e07147a126923ca6c32e
Alina <alex.alina.rusu@gmail.com>
2019-03-06 19:12:45 +0100

    removed browser's email input validation notification

--- src/components/form/SignupForm.js
+++ src/components/form/SignupForm.js
@@ -22,16 +22,23 @@ class SignupForm extends PureComponent {
   render() {
     return (
       <div className="signup-form">
-        <form className="form" onSubmit={this.handleSubmit}>
+        <form className="form" onSubmit={this.handleSubmit} noValidate>
           <label>
             Signup to get the latests updates <br></br>
             <input
+              className="form.control"
               type="email"
               name="email"
               value={this.state.email || ''}
               onChange={this.handleChange}
               placeholder="email@hello.com"
+              minlength="6"
+              maxlength="100"
+              pattern={/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/}
             />
+            <span className="invalid-feedback">
+              <p className="error-message">Enter a valid email address</p>
+            </span>
           </label>
           <input className="submit-btn" type="submit" value="Submit" />
         </form>
