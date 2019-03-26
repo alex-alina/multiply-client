9cd0c71e6b9afbc9309b9f112f2a6a3fd2311e39
Alina <alex.alina.rusu@gmail.com>
2019-03-26 12:28:53 +0100

    updated rendering for error and successful registration messages

--- src/components/form/SignupForm.js
+++ src/components/form/SignupForm.js
@@ -1,7 +1,7 @@
 import React, { PureComponent } from 'react';
 import './SignupForm.css';
 import FormValidationMessage from './FormValidationMessage';
-const regExPattern = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const regExPattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 class SignupForm extends PureComponent {
   state = {
@@ -9,8 +9,35 @@ class SignupForm extends PureComponent {
     disableSubmit: true,
   }
 
+  handleChange = (event) => {
+    const { name, value } = event.target;
+    this.setState({
+      confirmRegistration: false,
+      [name]: value,
+    });
+    this.validateEmail();
+  }
+  
+  // handleOnPaste = (event) => {
+    
+  // }
+
+  handleOnFocus = (event) => {
+    this.setState({
+      confirmRegistration: false,
+    });
+  }
+
+  handleOnBlur = (event) => {
+    this.setState({
+      confirmRegistration: false,
+      invalidEmail: false,
+      email: ""
+    });
+  }
+
   validateEmail = () => {
-    if (regExPattern.test(this.state.email)) {
+    if (regExPattern.test(this.state.email) && this.state.email !== "") {
       this.setState({
         invalidEmail: false,
         disableSubmit: false,
@@ -27,17 +54,9 @@ class SignupForm extends PureComponent {
     this.props.onSubmit(this.state);
     this.setState({
       email: "",
-      invalidEmail: "",
       confirmRegistration: true,
+      disableSubmit: true,
     });
-  }
-
-  handleChange = (event) => {
-    const { name, value } = event.target;
-    this.setState({
-      [name]: value,
-    });
-    this.validateEmail();
   }
 
   render() {
@@ -52,12 +71,15 @@ class SignupForm extends PureComponent {
               name="email"
               value={this.state.email || ''}
               onChange={this.handleChange}
+              // onPaste={this.handleOnPaste}
+              onFocus={this.handleOnFocus}
+              onBlur={this.handleOnBlur}
               placeholder="email@hello.com"
-              autocomplete="off"
+              autoComplete="off"
             />
           </label>
           { this.state.invalidEmail ? <FormValidationMessage color="#F26627" message="Please fill in a valid email" /> : null}
-          {this.state.confirmRegistration ? <FormValidationMessage color="#fff" message="Email registration successful." /> : null}
+          { this.state.confirmRegistration ? <FormValidationMessage color="#9CD7D1" message="Email registration successful." /> : null}
           <input className="submit-btn" type="submit" value="Submit" disabled={this.state.disableSubmit}/>
         </form>
       </div>
